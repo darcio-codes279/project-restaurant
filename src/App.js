0d58d8e9bbdd6204ccb7ea4313b3b16890709682
darcio-codes279 <darciomassala@gmail.com>
2022-10-15 21:21:59 +0100

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css'
+import { Nav, Navbar, NavDropdown} from 'react-bootstrap'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Navbar bg= "dark" variant='dark'
+        sticky='top' expand="lg" collapseOnSelect>
+          <Navbar.Brand>
+            <img src={logo} width='40px' height='40px'/>
+            Massala 
+          </Navbar.Brand>
+<Navbar.Toggle/>
+<Navbar.Collapse>
+<Nav>
+            <NavDropdown title="Products">
+              <NavDropdown.Item href='products/tea'>Tea</NavDropdown.Item>
+              <NavDropdown.Item href='products/coffee'>Coffee</NavDropdown.Item>
+              <NavDropdown.Item href='products/Hot Chocolate'>Hot Chocolate</NavDropdown.Item>
+              <NavDropdown.Divider/>
+              <NavDropdown.Item href='products/promo'>promo</NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link href='Blog'>Blog</Nav.Link>
+            <Nav.Link href='About us'>About us</Nav.Link>
+            <Nav.Link href='Contact us'>Contact us</Nav.Link>
+          </Nav>
+          <Navbar.Toggle/>
+          <Navbar.Collapse/>
+</Navbar.Collapse>
+      </Navbar>
+      <div className='content'>
+        This is a navigation bar
+      </div>
     </div>
   );
 }
